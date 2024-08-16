cb8d4678a0f209f7c9052fa1d5578a403115a2a9
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-08-16 01:59:21 -0400

    add css to home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,9 +6,15 @@ import "./Home.css"
 export default function Home() {
     return (
         <div className="Home">
+            <h1>Albums
             <Albums />
+            </h1>
+            <h1>Artists
             <Artists />
+            </h1>
+            <h1>Playlists
             <Playlists />
+            </h1>
         </div>
     )
 }
