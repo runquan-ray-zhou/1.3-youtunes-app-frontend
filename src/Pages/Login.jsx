415abd130d0b0f7f2441b9b1cf747ca7fbd6d50c
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-11-24 00:37:40 -0500

    change login to enter

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,27 +1,30 @@
 import { Link } from "react-router-dom";
-import "./Login.css"
+import "./Login.css";
 
 export default function Login() {
-    return (
+  return (
     <div className="Login">
-        <div className="circle">
-            <div className="circle1">
-                <div className="circle2">
-                    <div className="circle3">
-                    <img src="https://media4.giphy.com/media/4T7zBzdeNEtjThYDWn/giphy.gif?cid=790b76114ee03ef7f860492a9083d77f86191a7bf340002c&rid=giphy.gif&ct=g" alt="tuner icon" />
-                    </div>
-                </div>
+      <div className="circle">
+        <div className="circle1">
+          <div className="circle2">
+            <div className="circle3">
+              <img
+                src="https://media4.giphy.com/media/4T7zBzdeNEtjThYDWn/giphy.gif?cid=790b76114ee03ef7f860492a9083d77f86191a7bf340002c&rid=giphy.gif&ct=g"
+                alt="tuner icon"
+              />
             </div>
+          </div>
+        </div>
         <span className="login__app-name">Youtunes</span>
+      </div>
+      <div className="login-button__container">
+        <div className="edition">
+          <p>2024 EDITION</p>
         </div>
-        <div className="login-button__container">
-            <div className="edition">
-            <p>2024 EDITION</p>
-            </div>
-            <Link to="/home">
-                <button className="login-button">LOGIN</button>
-            </Link>
-        </div>
+        <Link to="/home">
+          <button className="login-button">ENTER</button>
+        </Link>
+      </div>
     </div>
-    )
+  );
 }
